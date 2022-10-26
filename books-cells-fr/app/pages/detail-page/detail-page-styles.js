6a7ed59c10c4e41a7acc239de35b8a40c1b78e43
Fayber Rueda <faybermauricio.rueda.contractor.cidentity@bbva.com>
2022-10-26 12:30:44 -0500

    add styles for detail-page

---
+++ books-cells-fr/app/pages/detail-page/detail-page-styles.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import { css, unsafeCSS } from "lit-element";
+import * as foundations from "@bbva-web-components/bbva-foundations-styles";
+
+export default css`.container {
+  align-items: center;
+  background-color: #f4f4f4;
+  display: flex;
+  height: 100%;
+  justify-content: center;
+  width: 100%;
+}
+`;
